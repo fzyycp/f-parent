44d2b458a181e9529eb495233b7c035e2b90ace2
fzyycp <yucai_fan@163.com>
2015-08-19 22:42:10 +0800

    f-samples-spring-schema

---
+++ f-samples/f-samples-spring-schema/src/main/java/cn/fzyycp/samples/bean/People.java
@@ -0,0 +1,19 @@
+/**
+ * fzyycp项目
+ * 
+ * @author Fanyucai
+ * @date 2015年8月19日
+ */
+package cn.fzyycp.samples.bean;
+
+/**
+ * 用户Bean
+ * 
+ * @author Fanyucai
+ *
+ */
+public class People {
+	private String id;  
+    private String name;  
+    private Integer age; 
+}
